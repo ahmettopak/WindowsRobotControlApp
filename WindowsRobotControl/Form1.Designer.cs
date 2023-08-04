c3b6d74b1508c761deae61a7307488fb5cd93bfd
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-08-04 13:27:24 +0300

    Rtsp stream add

---
+++ WindowsRobotControl/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsRobotControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.leftVideoPanel = new System.Windows.Forms.Panel();
+            this.leftFullSizeButton = new System.Windows.Forms.Button();
+            this.leftCameraComboBox = new System.Windows.Forms.ComboBox();
+            this.rightVideoPanel = new System.Windows.Forms.Panel();
+            this.rightFullSizeButton = new System.Windows.Forms.Button();
+            this.rightCameraComboBox = new System.Windows.Forms.ComboBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.leftVideoPanel.SuspendLayout();
+            this.rightVideoPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsRobotControl.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(831, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(377, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // leftVideoPanel
+            // 
+            this.leftVideoPanel.Controls.Add(this.leftFullSizeButton);
+            this.leftVideoPanel.Controls.Add(this.leftCameraComboBox);
+            this.leftVideoPanel.Location = new System.Drawing.Point(12, 99);
+            this.leftVideoPanel.Name = "leftVideoPanel";
+            this.leftVideoPanel.Size = new System.Drawing.Size(1000, 558);
+            this.leftVideoPanel.TabIndex = 1;
+            // 
+            // leftFullSizeButton
+            // 
+            this.leftFullSizeButton.BackColor = System.Drawing.Color.Transparent;
+            this.leftFullSizeButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("leftFullSizeButton.BackgroundImage")));
+            this.leftFullSizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.leftFullSizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.leftFullSizeButton.ForeColor = System.Drawing.Color.Transparent;
+            this.leftFullSizeButton.Location = new System.Drawing.Point(136, 3);
+            this.leftFullSizeButton.Name = "leftFullSizeButton";
+            this.leftFullSizeButton.Size = new System.Drawing.Size(50, 50);
+            this.leftFullSizeButton.TabIndex = 1;
+            this.leftFullSizeButton.UseVisualStyleBackColor = false;
+            this.leftFullSizeButton.Click += new System.EventHandler(this.leftFullSizeButton_Click);
+            // 
+            // leftCameraComboBox
+            // 
+            this.leftCameraComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.leftCameraComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftCameraComboBox.FormattingEnabled = true;
+            this.leftCameraComboBox.Location = new System.Drawing.Point(3, 3);
+            this.leftCameraComboBox.Name = "leftCameraComboBox";
+            this.leftCameraComboBox.Size = new System.Drawing.Size(130, 44);
+            this.leftCameraComboBox.TabIndex = 0;
+            this.leftCameraComboBox.SelectedIndexChanged += new System.EventHandler(this.leftCameraComboBox_SelectedIndexChanged);
+            // 
+            // rightVideoPanel
+            // 
+            this.rightVideoPanel.Controls.Add(this.rightFullSizeButton);
+            this.rightVideoPanel.Controls.Add(this.rightCameraComboBox);
+            this.rightVideoPanel.Location = new System.Drawing.Point(1025, 99);
+            this.rightVideoPanel.Name = "rightVideoPanel";
+            this.rightVideoPanel.Size = new System.Drawing.Size(1000, 558);
+            this.rightVideoPanel.TabIndex = 2;
+            // 
+            // rightFullSizeButton
+            // 
+            this.rightFullSizeButton.BackColor = System.Drawing.Color.Transparent;
+            this.rightFullSizeButton.BackgroundImage = global::WindowsRobotControl.Properties.Resources.fullsize_right;
+            this.rightFullSizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rightFullSizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.rightFullSizeButton.Location = new System.Drawing.Point(814, 3);
+            this.rightFullSizeButton.Name = "rightFullSizeButton";
+            this.rightFullSizeButton.Size = new System.Drawing.Size(50, 50);
+            this.rightFullSizeButton.TabIndex = 1;
+            this.rightFullSizeButton.UseVisualStyleBackColor = false;
+            this.rightFullSizeButton.Click += new System.EventHandler(this.rightFullSizeButton_Click);
+            // 
+            // rightCameraComboBox
+            // 
+            this.rightCameraComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.rightCameraComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightCameraComboBox.FormattingEnabled = true;
+            this.rightCameraComboBox.Location = new System.Drawing.Point(867, 3);
+            this.rightCameraComboBox.Name = "rightCameraComboBox";
+            this.rightCameraComboBox.Size = new System.Drawing.Size(130, 44);
+            this.rightCameraComboBox.TabIndex = 0;
+            this.rightCameraComboBox.SelectedIndexChanged += new System.EventHandler(this.rightCameraComboBox_SelectedIndexChanged);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.BackColor = System.Drawing.Color.Transparent;
+            this.refreshButton.BackgroundImage = global::WindowsRobotControl.Properties.Resources.refresh;
+            this.refreshButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.refreshButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.refreshButton.ForeColor = System.Drawing.Color.Transparent;
+            this.refreshButton.Location = new System.Drawing.Point(991, 102);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(50, 50);
+            this.refreshButton.TabIndex = 3;
+            this.refreshButton.UseVisualStyleBackColor = false;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(2382, 1303);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.rightVideoPanel);
+            this.Controls.Add(this.leftVideoPanel);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Robot Control App";
+            this.TransparencyKey = System.Drawing.Color.DarkGray;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.leftVideoPanel.ResumeLayout(false);
+            this.rightVideoPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel leftVideoPanel;
+        private System.Windows.Forms.Panel rightVideoPanel;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.ComboBox leftCameraComboBox;
+        private System.Windows.Forms.ComboBox rightCameraComboBox;
+        private System.Windows.Forms.Button leftFullSizeButton;
+        private System.Windows.Forms.Button rightFullSizeButton;
+    }
+}
+
---
+++ WindowsRobotControl/Form1.cs
@@ -0,0 +1,229 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vlc.DotNet.Forms;
+
+namespace WindowsRobotControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCombobox();
+        }
+        string[] cameraUrl = {
+                "rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?",
+                "rtsp://192.168.1.10:554/user=admin&password=&channel=2&stream=0.sdp?",
+                "rtsp://192.168.1.10:554/user=admin&password=&channel=3&stream=0.sdp?",
+                "rtsp://192.168.1.10:554/user=admin&password=&channel=4&stream=0.sdp?",
+                "rtsp://192.168.1.10:554/user=admin&password=&channel=5&stream=0.sdp?"
+            };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeVlcControl();
+            leftCameraComboBox.SelectedIndex = 0;
+            rightCameraComboBox.SelectedIndex = 0;
+
+            //InitializeController();
+            //SetupControllerTimer();
+
+            //Right Video View Full Size
+            //panel1.SendToBack();
+            //panel1.Size = this.ClientSize;
+        }
+
+        #region VlcStream
+
+
+        private VlcControl vlcControlLeft;
+        private VlcControl vlcControlRight;
+
+        private void InitializeVlcControl()
+        {
+            vlcControlLeft = new VlcControl();
+            vlcControlRight = new VlcControl();
+
+            var vlcArgs = new List<string>
+    {
+        "--no-plugins-cache",
+        "--no-video-title-show",
+        "--no-snapshot-preview",
+        "--file-caching=0",
+        "--clock-jitter=0",
+        "--network-caching=80",
+        "--live-caching=0",
+        "--no-overlay",
+        "--clock-jitter=0"
+    };
+
+
+            vlcControlLeft.BeginInit();
+            vlcControlLeft.VlcMediaplayerOptions = vlcArgs.ToArray();
+            vlcControlLeft.VlcLibDirectory = new DirectoryInfo(@"C:\Program Files (x86)\VideoLAN\VLC"); // VLC kütüphane dizini
+            vlcControlLeft.EndInit();
+            vlcControlLeft.Dock = DockStyle.Fill;
+            leftVideoPanel.Controls.Add(vlcControlLeft); // RTSP yayınını göstermek için bir Panel kontrolü ekleyin
+
+
+            vlcControlRight.BeginInit();
+            vlcControlRight.VlcMediaplayerOptions = vlcArgs.ToArray();
+            vlcControlRight.VlcLibDirectory = new DirectoryInfo(@"C:\Program Files (x86)\VideoLAN\VLC"); // VLC kütüphane dizini
+            vlcControlRight.EndInit();
+            vlcControlRight.Dock = DockStyle.Fill;
+            rightVideoPanel.Controls.Add(vlcControlRight); // RTSP yayınını göstermek için bir Panel kontrolü ekleyin
+        }
+
+        private void StartLeftVideo(string rtspUrl)
+        {
+            if (vlcControlLeft != null)
+            {
+                vlcControlLeft.Stop(); // Önceki medya durduruluyor
+                vlcControlLeft.Play(new Uri(rtspUrl));
+            }
+
+
+        }
+
+        private void StartRightVideo(string rtspUrl)
+        {
+            if (vlcControlRight != null)
+            {
+                vlcControlRight.Stop(); // Önceki medya durduruluyor
+                vlcControlRight.Play(new Uri(rtspUrl));
+            }
+        }
+
+        private void StopCapture()
+        {
+            if (vlcControlLeft != null)
+            {
+                vlcControlLeft.Stop();
+            }
+        }
+        #endregion 
+
+
+        #region Joystick
+        private Controller controller;
+        private UserIndex playerIndex = UserIndex.One; // Eğer birden fazla controller varsa, uygun playerIndex'i seçin.
+        private Timer controllerTimer;
+        private void InitializeController()
+        {
+            try
+            {
+                controller = new Controller(playerIndex);
+                if (!controller.IsConnected)
+                {
+                    MessageBox.Show("Controller bulunamadı!");
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Bulundu");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Controller başlatılırken bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void ReadControllerInput()
+        {
+            if (controller == null || !controller.IsConnected)
+                return;
+
+            Gamepad gamepadState = controller.GetState().Gamepad;
+
+            // Gamepad verilerini işleyin
+            short leftThumbX = gamepadState.LeftThumbX;
+            short leftThumbY = gamepadState.LeftThumbY;
+            short rightThumbX = gamepadState.RightThumbX;
+            short rightThumbY = gamepadState.RightThumbY;
+            byte leftTrigger = gamepadState.LeftTrigger;
+            byte rightTrigger = gamepadState.RightTrigger;
+            bool buttonA = (gamepadState.Buttons & GamepadButtonFlags.A) != 0;
+            bool buttonB = (gamepadState.Buttons & GamepadButtonFlags.B) != 0;
+            bool buttonX = (gamepadState.Buttons & GamepadButtonFlags.X) != 0;
+            bool buttonY = (gamepadState.Buttons & GamepadButtonFlags.Y) != 0;
+
+            // label1.Text = NormalizeAxisValue(leftThumbX)  + " " + NormalizeAxisValue(leftThumbY);
+        }
+
+
+
+        private void SetupControllerTimer()
+        {
+            controllerTimer = new Timer();
+            controllerTimer.Interval = 100; // 100 ms aralıklarla veri alacak
+            controllerTimer.Tick += (sender, e) => ReadControllerInput();
+            controllerTimer.Start();
+        }
+
+        private float NormalizeAxisValue(short axisValue)
+        {
+            const float minAxisValue = -32768f;
+            const float maxAxisValue = 32767f;
+
+            return (axisValue - minAxisValue) / ((maxAxisValue - minAxisValue) / 2f) - 1f;
+        }
+
+        #endregion
+
+        private void InitializeCombobox()
+        {
+            string[] cameraName = { "Ön", "Arka", "Nişan", "Kıskaç", "PTZ" };
+
+
+            leftCameraComboBox.Items.Clear();
+            foreach (string value in cameraName)
+            {
+                leftCameraComboBox.Items.Add(value);
+            }
+
+            rightCameraComboBox.Items.Clear();
+            foreach (string value in cameraName)
+            {
+                rightCameraComboBox.Items.Add(value);
+            }
+
+
+        }
+
+        private void leftCameraComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StartLeftVideo(cameraUrl[leftCameraComboBox.SelectedIndex]);
+        }
+
+        private void rightCameraComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StartRightVideo(cameraUrl[rightCameraComboBox.SelectedIndex]);
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            StartLeftVideo(cameraUrl[leftCameraComboBox.SelectedIndex]);
+            StartRightVideo(cameraUrl[rightCameraComboBox.SelectedIndex]);
+        }
+
+        private void rightFullSizeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftFullSizeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
